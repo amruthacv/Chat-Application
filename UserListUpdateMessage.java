9dd344d5aac60e778a76831ac0cf9b1419e1d6d3
amruthacv <cvvamrutha2010@gmail.com>
2015-07-08 14:44:16 +0530

    Assignment1

---
+++ UserListUpdateMessage.java
@@ -0,0 +1,16 @@
+package chatapp;
+
+
+import java.util.List;
+
+public class UserListUpdateMessage extends StructuredMessage {
+
+    public UserListUpdateMessage(List usernames) {
+        super(ChatMessage.USERLIST_UPDATE, usernames);
+    }
+
+    public List getUserList() {
+        return super.dataList;
+    }
+
+}
